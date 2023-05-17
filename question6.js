277a32e8f902830fd258f232c71c28e02bfd497c
g19m7057 <sifisom599@gmail.com>
2023-05-17 09:06:49 +0200

    initial commit

---
+++ question6.js
@@ -0,0 +1,34 @@
+const stats = (statsNumberList) =>{
+    let sortedArr = statsNumberList.sort();
+    let median = sortedArr[sortedArr.length/2];
+    let highestNumber = sortedArr[sortedArr.length-1];
+    let lowestNumber = sortedArr[0];
+
+    let list = {};
+    let maximum = 0, mode;
+    let modes = [];
+
+    sortedArr.forEach(element => {
+        if(list[element]){
+            list[element]++;
+        }
+        else{
+            list[element] = 1;
+        }
+        if(list[element] > maximum){
+            maximum = list[element];
+            mode = element;
+        }
+    });
+
+    //look for others with the same occurence to mode
+    for(element in list){
+        if(list[mode] === list[element]){
+            modes.push(element);
+        }
+    }
+    modes = modes.join(', ');
+    return {median, highestNumber, lowestNumber, modes};
+}
+
+console.log(stats([1,4,3,4,5,7,7,8,9,10]))
---
+++ question7.js
@@ -0,0 +1,19 @@
+const decrypt = (string) =>{
+    const alphabetLowerCase = [];
+
+    //create alphabet from ascii code
+    for (let i = 97; i <= 122; i++) {
+        alphabetLowerCase.push(String.fromCharCode(i));
+    }
+    let strArr = string.split('');
+    for(let i=0; i<strArr.length; i++){
+        if(alphabetLowerCase.includes(strArr[i])){
+            let element1 = alphabetLowerCase.indexOf(strArr[i]);
+            strArr[i] = alphabetLowerCase[25-element1]
+        }
+    }
+    return strArr.join('');
+}
+
+console.log(decrypt("Drw blf hvv ozhg mrtsg'h vkrhlwv?"));
+console.log(decrypt('Yvzs! I xzm\'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!'))
